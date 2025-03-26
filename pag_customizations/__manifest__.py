78d32911996ad9579048ab58a82904c53800170c
Sonali Trinesis <sonalip@trinesis.com>
2025-03-26 13:52:24 +0530

    PG-17-Scorecard-Proof-of-Concept

--- pag_customizations/__manifest__.py
+++ pag_customizations/__manifest__.py
@@ -9,7 +9,7 @@ Module containing customizations for Precision Aviation Group
     'author': "Beareded Moose Technologies",
     'website': "https://www.beardedmoosetech.com",
     'category': 'Project',
-    'depends': ['project','web'],
+    'depends': ['project','web','web_hierarchy'],
     'data': [
             "security/ir.model.access.csv",
             "data/progress_task.xml",
